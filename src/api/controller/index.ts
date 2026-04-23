29c1fe208c713be5ae385aaedfaa5e10b0c6d90a
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-23 19:40:27 -0300

    first commit

---
+++ src/api/controller/index.ts
@@ -0,0 +1,10 @@
+// import { faker } from '@faker-js/faker';
+// import User from '../model/User.ts';
+
+const store = (req: any, res: any) => {
+    return res.json({
+        "status": "OK"
+    })
+}
+
+export default { store }
