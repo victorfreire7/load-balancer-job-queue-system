8488861e8ef07d1759cfc850a68fa3416e743fb8
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-23 20:11:04 -0300

    first store mongodb

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -1,10 +1,17 @@
-// import { faker } from '@faker-js/faker';
-// import User from '../model/User.ts';
+import { faker } from '@faker-js/faker';
+import User from '../model/User.ts';
 
-const store = (req: any, res: any) => {
-    return res.json({
-        "status": "OK"
-    })
+const store = async (req: any, res: any) => {
+    try {
+        const user = await User.create({
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        });
+
+        return res.json(user);
+    } catch (error) {
+        console.error(`ERRO: ${error}`)        
+    }
 }
 
 export default { store }
