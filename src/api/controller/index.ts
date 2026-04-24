7eaf0ea3b562ab43b40d0ed924101b6120c6d052
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-24 17:16:04 -0300

    create handle_1 & handle_2 w/ reid

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -1,17 +1,53 @@
+import redis from '../config/redis.ts';
 import { faker } from '@faker-js/faker';
-import User from '../model/User.ts';
+// import User from '../model/User.ts';
+
+
+        // 1. criar set: count + "email pass"
+        // 2. verificar qual handle esta mais disponivel
+        // 3. adicionar um append no value com o handle que esta mais vazio
+        // 4. adicionar 1 no contador
 
 const store = async (req: any, res: any) => {
     try {
-        const user = await User.create({
-            email: faker.internet.email(),
-            password: faker.internet.password()
-        });
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        const info:any = `${email} ${password}`;
 
-        return res.json(user);
+        let handle:string | any = 'HANDLE_2';
+
+        await redis.lPush(
+            handle, 
+            info 
+        );
+
+        const HANDLE_1 = await redis.lRange("HANDLE_1", 0, -1) // RETORNO = ARRAY
+        const HANDLE_2 = await redis.lRange("HANDLE_2", 0, -1) // RETORNO = ARRAY
+
+        console.log(HANDLE_1)
+        console.log(HANDLE_2)
+
+        res.json({
+            "HANDLE": handle,
+            "EMAIL": email, 
+            "PASSWORD": password
+        })
+
     } catch (error) {
-        console.error(`ERRO: ${error}`)        
+        console.error(`ERRO: ${error}`)
     }
+  
+  
+    // try {
+    //     const user = await User.create({
+    //         email: faker.internet.email(),
+    //         password: faker.internet.password()
+    //     });
+
+    //     return res.json(user);
+    // } catch (error) {
+    //     console.error(`ERRO: ${error}`)        
+    // }
 }
 
 export default { store }
