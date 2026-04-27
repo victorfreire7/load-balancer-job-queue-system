ca03d9f8d4db51b7e19d860eee0e9285d99bc9d1
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-27 12:42:01 -0300

    change redis host - move to dotenv

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -4,12 +4,13 @@ config();
 
 const redis_user:any = process.env.REDIS_USER
 const redis_password:any = process.env.REDIS_PASSWORD
+const redis_host:any = process.env.REDIS_HOST
 
 const client = createClient({
     username: redis_user,
     password: redis_password,
     socket: {
-        host: 'redis-13460.c308.sa-east-1-1.ec2.cloud.redislabs.com',
+        host: redis_host,
         port: 13460
     }
 });
