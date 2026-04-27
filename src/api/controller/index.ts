4dc2c5201d6755b52dd686a8846d8042d5b0a9b4
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-27 00:05:22 -0300

    queue selector handler changed

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -2,6 +2,9 @@ import redis from '../../config/redis.ts';
 import { faker } from '@faker-js/faker';
 let i = 0
 
+const random_num = (min : number, max : number) => {
+    return Math.round(Math.random() * (max - min) + min);
+}
 
 const store = async (req: any, res: any) => {
     while (i < 1000) {        
@@ -9,14 +12,10 @@ const store = async (req: any, res: any) => {
             const email = faker.internet.email();
             const password = faker.internet.password();
             const info:any = `${email} ${password}`;
+            const rand: number = random_num(1, 4)
     
-            const HANDLE_1 = await redis.lRange("HANDLE_1", 0, -1) // RETORNO = ARRAY
-            const HANDLE_2 = await redis.lRange("HANDLE_2", 0, -1) // RETORNO = ARRAY
-    
-            let handle:string | any = 'HANDLE_1';
-            if(HANDLE_1.length > HANDLE_2.length){            
-                handle = 'HANDLE_2';
-            }
+            let handle:string | any = `HANDLE_${rand}`;
+      
     
             // LPUSH (HANDLE_1 || HANDLE_2) "EMAIL PASSWORD"
     
