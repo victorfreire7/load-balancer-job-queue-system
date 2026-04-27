9236bff1f623cfebd355859c385047cb704fcdf3
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-27 12:59:17 -0300

    change 'split' to 'JSON.stringdy/parse'

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -1,28 +1,34 @@
 import redis from '../../config/redis.ts';
 import { faker } from '@faker-js/faker';
 let i = 0;
-const handler_numbs: number = 2
+const handler_numbs: number = 1;
+
+type User = {
+    email: string,
+    password: string
+}
 
 const random_num = (min : number, max : number) => {
     return Math.round(Math.random() * (max - min) + min);
 }
 
 const store = async (req: any, res: any) => {
-    while (i < 1000) {        
+    while (i < 1) {        
         try {
             const email = faker.internet.email();
             const password = faker.internet.password();
-            const info:any = `${email} ${password}`;
+
             const rand: number = random_num(0.59, handler_numbs + 0.49)
     
-            let handle:string | any = `HANDLE_${rand}`;
-      
-    
-            // LPUSH (HANDLE_1 || HANDLE_2) "EMAIL PASSWORD"
+            const user: User = {
+                email: email,
+                password: password
+            }
+
     
             await redis.lPush(
-                handle, 
-                info 
+                `HANDLE_${rand}`, 
+                JSON.stringify(user) 
             );
             i++; 
             continue  
--- src/handler/handler.one.ts
+++ src/handler/handler.one.ts
@@ -15,27 +15,30 @@ await redis.connect()
 await mongodb()
 .then(() => console.log(`mongodb connect in port ${port}`));
 
+type User = {
+    email: string,
+    password: string
+}
+
 app.listen(port, async (): Promise<void> => { 
     while (true) {   
-        let HANDLE_1: string[] | null = await redis.lRange(handler_name, 0, -1) // RETORNO = ARRAY
+        let HANDLE_1: string[] | null = await redis.lRange(handler_name, 0, -1) // BUSCANDO O TAMANHO DO HANDLER RETORNO = ARRAY
         
         if(HANDLE_1.length > 0){
-            const user: any = await redis.lmPop( // LRANGE HANDLE_2 0 -1
+            const user: any = await redis.lmPop( // como lmpush guarda itens em formato de pilha, eu estou buscando o item da direita para esquerda (do ultimo para o primeiro)
                 handler_name,
                 'RIGHT',
             ); // retorno: [ 'HANDLE_2', [ 'Katrina.Grady29@gmail.com G9KHj1MpR4aaEto' ] ]
-    
-            const user_split: string[] = user[1][0]?.split(' '); // retorno: ['email', 'password']
-            let user_email: any = user_split[0];
-            let user_password: any = user_split[1];
-        
-            console.log(user_split)
-    
+            
+            const user_json: User = JSON.parse(user[1][0]);
+
+            console.log(user_json)
+            
             await mongo_schema.create({ // estou buscando o schema de dentro da API; talvez nao seja a melhor maneira de fazer isso.
-                email: user_email,
-                password: user_password
+                email: user_json.email,
+                password: user_json.password
             });
-        } else {
+    } else {
             continue;
         }
     }
